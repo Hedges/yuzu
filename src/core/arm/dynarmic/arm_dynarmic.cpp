24921f89dc2ff70ca184a0490d591ecd75f40334
Jarek Syrylak <jarek.syrylak@gogoameo.com>
2018-08-06 17:29:24 +0100

    Remove unused include

--- src/core/arm/dynarmic/arm_dynarmic.cpp
+++ src/core/arm/dynarmic/arm_dynarmic.cpp
@@ -10,7 +10,6 @@
 #include "core/arm/dynarmic/arm_dynarmic.h"
 #include "core/core.h"
 #include "core/core_timing.h"
-#include "core/gdbstub/gdbstub.h"
 #include "core/hle/kernel/process.h"
 #include "core/hle/kernel/svc.h"
 #include "core/memory.h"
