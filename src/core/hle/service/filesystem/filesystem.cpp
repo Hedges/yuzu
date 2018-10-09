d934fbfd8d943dd29bf579c3905d7fda59f6ca0d
Hedges <hedges@resync.pl>
2018-10-09 17:32:17 +0100

    Argh! I do need it to experiment with NROs!

--- src/core/hle/service/filesystem/filesystem.cpp
+++ src/core/hle/service/filesystem/filesystem.cpp
@@ -237,7 +237,7 @@ static std::unique_ptr<FileSys::SDMCFactory> sdmc_factory;
 static std::unique_ptr<FileSys::BISFactory> bis_factory;
 
 ResultCode RegisterRomFS(std::unique_ptr<FileSys::RomFSFactory>&& factory) {
-    ASSERT_MSG(romfs_factory == nullptr, "Tried to register a second RomFS");
+    //ASSERT_MSG(romfs_factory == nullptr, "Tried to register a second RomFS");
     romfs_factory = std::move(factory);
     LOG_DEBUG(Service_FS, "Registered RomFS");
     return RESULT_SUCCESS;
@@ -381,6 +381,14 @@ void CreateFactories(const FileSys::VirtualFilesystem& vfs, bool overwrite) {
         save_data_factory = std::make_unique<FileSys::SaveDataFactory>(std::move(nand_directory));
     if (sdmc_factory == nullptr)
         sdmc_factory = std::make_unique<FileSys::SDMCFactory>(std::move(sd_directory));
+
+    if (romfs_factory == nullptr) {
+        if (&(Core::System::GetInstance().GetAppLoader()) != nullptr) {
+            auto romfs =
+                std::make_unique<FileSys::RomFSFactory>(Core::System::GetInstance().GetAppLoader());
+            romfs_factory = std::move(romfs);
+        }
+    }
 }
 
 void InstallInterfaces(SM::ServiceManager& service_manager, const FileSys::VirtualFilesystem& vfs) {
